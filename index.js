d0e4d61e11ab19745ca7f9d6acc47e30342bab1b
AbA1oNe <louis2001145145@gmail.com>
2024-02-06 20:38:28 +0800

    check Nan

--- index.js
+++
@@ -1,8 +0,0 @@
-let http = require("http");
-let fs = require("fs");
-
-http.createServer(function(req,res) {
-    let html = fs.readFileSync(`${__dirname}/index.html`);
-    res.writeHead(200, {"contentType":"text/html"});
-    res.end(html);
-  }).listen(1324,"127.0.0.1");
